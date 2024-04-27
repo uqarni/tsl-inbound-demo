5517741774bb1e89ac2f89c509e4d685aecaa0f1
uqarni <108438834+uqarni@users.noreply.github.com>
2024-04-27 16:52:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,32 +27,32 @@ def main():
     phone = st.text_input("Lead Phone", value="282-222-3333")
     lastname = st.text_input("Lead Last Name", value="Doe")
     firstname = st.text_input("Lead First Name", value="John")
-    what_is_your_age_ = st.text_input("Lead Age", value="20")
+    what_is_your_age_ = st.text_input("Lead Age", value="")
     
     income_options = ["Unemployed", "$0 - $50K", "$50K - $100K", "$100K - $150K", "$150K - $225K", "$225K+"]
     what_is_your_current_annual_income_ = st.selectbox("Lead Annual Income", options=income_options, index=2)
     
-    how_did_you_originally_hear_about_us_ = st.text_input("Lead Discovery Source", value="LinkedIn")
+    how_did_you_originally_hear_about_us_ = st.text_input("Lead Discovery Source", value="")
     which_job_title_do_you_most_identify_with_ = st.text_input("Lead Job Title", value="Director")
 
-    paid_options = ["0", "1-10", "11-50", "50+"]
-    how_many_times_have_you_been_paid_to_speak_ = st.selectbox("Times Paid to Speak", options = paid_options, index = 2)
+    paid_options = ["", "0", "1-10", "11-50", "50+"]
+    how_many_times_have_you_been_paid_to_speak_ = st.selectbox("Times Paid to Speak", options = paid_options, index = 0)
     
     what_is_your_highest_level_of_education_completed = st.text_input("Lead Education Level", value="Bachelors")
 
-    speaking_income = ["$0","Under $1,000","$1,000 - $5,000","$5,000 - $15,000","$15,000+"]
-    how_much_do_you_currently_make_per_month_speaking_ = st.selectbox("Lead Monthly Speaking Income", options = speaking_income, index = 3)
+    speaking_income = ["", "$0","Under $1,000","$1,000 - $5,000","$5,000 - $15,000","$15,000+"]
+    how_much_do_you_currently_make_per_month_speaking_ = st.selectbox("Lead Monthly Speaking Income", options = speaking_income, index = 0)
 
-    industry_options = ["Government/Military","Non-Profit","Corporate","Association","Church/Faith-Based","College/University","Education (K-12)","Other"]
-    what_industry_do_you_speak_in_or_hope_to_speak_in_ = st.selectbox("Lead Speaking Industry", options = industry_options, index = 1)
+    industry_options = ["","Government/Military","Non-Profit","Corporate","Association","Church/Faith-Based","College/University","Education (K-12)","Other"]
+    what_industry_do_you_speak_in_or_hope_to_speak_in_ = st.selectbox("Lead Speaking Industry", options = industry_options, index = 0)
     
-    please_list_your_speaking_website_or_linkedin_profile_here_ = st.text_input("Lead Speaking Website/LinkedIn", value="www.johndoespeaking.com")
+    please_list_your_speaking_website_or_linkedin_profile_here_ = st.text_input("Lead Speaking Website/LinkedIn", value="")
 
-    desired_income = ["$1,000 - $5,000","$5,000 - $10,000","$10,000 - $20,000","$20,000+"]
-    what_is_your_desired_monthly_income_from_speaking_what_is_your_goal_ = st.selectbox("Lead Desired Monthly Income from Speaking", options = desired_income, index = 3)
+    desired_income = ["", "$1,000 - $5,000","$5,000 - $10,000","$10,000 - $20,000","$20,000+"]
+    what_is_your_desired_monthly_income_from_speaking_what_is_your_goal_ = st.selectbox("Lead Desired Monthly Income from Speaking", options = desired_income, index = 0)
                                                                                        
-    on_a_scale_of_1_10_how_commited_and_ready_are_you_to_invest_in_yourself_in_order_to_get_booked_and = st.text_input("Lead Commitment Level", value="9")
-    what_is_your_biggest_challenge_in_hitting_your_monthly_income_goal_of3_000___5_000_from_speaking = st.text_input("Lead Biggest Challenge to Hitting Monthly Goal", value="I don't know how to get more clients")
+    on_a_scale_of_1_10_how_commited_and_ready_are_you_to_invest_in_yourself_in_order_to_get_booked_and = st.text_input("Lead Commitment Level", value="")
+    what_is_your_biggest_challenge_in_hitting_your_monthly_income_goal_of3_000___5_000_from_speaking = st.text_input("Lead Biggest Challenge to Hitting Monthly Goal", value="")
 
     prompt_variables = {
         "name": name,
